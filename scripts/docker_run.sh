8a258d886b336875e1c11195c338041743229cf5
Robert Dates <rsndates@yahoo.com>
2018-06-28 20:02:54 -0400

    final

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-docker run -it -p 80:8080 --rm -v "$PWD":/application rdates/blockbuster:latest sh -c "$@"
+docker run -it  -p 8080:8080 -p 8100:8100 --rm --name movies -v "$PWD":/application rdates/blockbuster:latest
 
